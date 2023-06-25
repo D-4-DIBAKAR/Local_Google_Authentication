b69410bc30ead57493cda1a3c812b2ef3ce5dbf9
DIBAKAR PARIDA <71878062+D-4-DIBAKAR@users.noreply.github.com>
2023-06-26 03:53:11 +0530

    Update mongoose.js
    DB/node-authentication added

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 //CONNECTING TO SERVER
 require('dotenv').config();
 exports.connectMongoose = () => {
-    mongoose.connect(process.env.DATABASE)
+    mongoose.connect("mongodb://127.0.0.1:27017/node-authentication")
         .then(() => {
             console.log(`Connected Succesfully To Database.`);
         })
