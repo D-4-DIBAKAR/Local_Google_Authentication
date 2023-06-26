4fbbce9b3702ff19f6a4b3362b7386741d28c857
DIBAKAR PARIDA <71878062+D-4-DIBAKAR@users.noreply.github.com>
2023-06-26 07:44:19 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 //CONNECTING TO SERVER
 require('dotenv').config();
 exports.connectMongoose = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/node-authentication")
+    mongoose.connect(process.env.DATABASE)
         .then(() => {
             console.log(`Connected Succesfully To Database.`);
         })
