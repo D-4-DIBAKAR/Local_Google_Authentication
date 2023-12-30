811438c78d3337172d990283d39fa348ebfb22bb
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2023-12-30 19:07:36 +0530

    Fix : OAuth2

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-//this code is in new brach and not in master branch 
-const express = require('express');
+//this code is in new brach and not in master branch
+const express = require("express");
 const app = express();
-const env = require('dotenv').config();
+const env = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 // console.log(process.env);
-const flash = require('connect-flash');
-const path = require('path');
-const index_routes = require('./routes/home_routes');
-const user_routes = require('./routes/user_routes');
-const bcrypt = require('bcrypt');
-const { connectMongoose } = require('./config/mongoose')
-const ejs = require('ejs');
-const { check } = require('express-validator/check');
-const { validationResult } = require('express-validator/check');
-const { User } = require('./models/user');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const { initializingPassport } = require('./config/passport-local-strategy');
-const expressSession = require('express-session');
-const { isAuthenticated } = require('./config/passport-local-strategy');
-const { error } = require('console');
-const googleOauth2 = require('./config/passport-google-oauth2-strategy');
-require('dotenv').config();
-
+const flash = require("connect-flash");
+const path = require("path");
+const index_routes = require("./routes/home_routes");
+const user_routes = require("./routes/user_routes");
+const bcrypt = require("bcrypt");
+const { connectMongoose } = require("./config/mongoose");
+const ejs = require("ejs");
+const { check } = require("express-validator/check");
+const { validationResult } = require("express-validator/check");
+const { User } = require("./models/user");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { initializingPassport } = require("./config/passport-local-strategy");
+const expressSession = require("express-session");
+const { isAuthenticated } = require("./config/passport-local-strategy");
+const { error } = require("console");
+const googleOauth2 = require("./config/passport-google-oauth2-strategy");
+require("dotenv").config();
 
 //MIDDLEWARES
 app.use(flash());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-app.use(expressSession({
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  expressSession({
     secret: "secret",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
-}));
+    cookie: { secure: false },
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.set('view-engine', 'ejs');
-
+app.set("view-engine", "ejs");
 
 //CALLING THE FUNCTIONS
 const startMongoose = connectMongoose();
@@ -51,6 +51,5 @@ app.use(index_routes);
 
 //APP LISTEN
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
-
